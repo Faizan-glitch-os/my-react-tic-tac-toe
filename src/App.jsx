b11698aa57b16ed05309d25aa08177802195e5df
Faizan Ahmad <faizanahmad2582001@gmail.com>
2024-07-15 15:06:20 +0500

    till backdrop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import PlayerNamingCard from "./player-naming-card.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      {/* <aside className="backdrop"></aside> */}
+      <section className="player-naming-section">
+        <PlayerNamingCard />
+        <PlayerNamingCard />
+      </section>
+    </>
+  );
 }
 
 export default App;
---
+++ src/player-naming-card.jsx
@@ -0,0 +1,10 @@
+export default function PlayerNamingCard() {
+  return (
+    <div className="player-naming-card">
+      <h3>Player-1</h3>
+      <h2>Player Name</h2>
+      <h2>Player Symbol</h2>
+      <button>Rename</button>
+    </div>
+  );
+}
